47ea231c85675b00eceb0c0c80c66108deb6d15f
DmytroLymar <dmytrolymar901@gmail.com>
2026-01-14 17:42:46 +0200

    (education 4.4.2) Passing Arguments

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const schema = buildSchema(`
     type Query {
         quoteOfTheDay: String
         random: Float!
-        rollThreeDice: [Int]
+        rollDice(numDice: Int!, numSides: Int): [Int]
     }    
 `);
 
@@ -17,8 +17,12 @@ const root = {
     random() {
         return Math.random();
     },
-    rollThreeDice() {
-        return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+    rollDice({ numDice, numSides }) {
+        const output = [];
+        for (let i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
     }
 };
 
