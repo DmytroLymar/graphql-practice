472758801472af6666671d51132db267baab86b1
Dmytro Lymar <dmytrolymar901@gmail.com>
2026-01-15 10:12:29 +0200

    (education 4.4.4) Mutations and Input Types

--- src/resolvers/root.js
+++ src/resolvers/root.js
@@ -1,23 +1,29 @@
-class RandomDie {
-    constructor(numSides) {
-        this.numSides = numSides;
-    }
-
-    rollOnce() {
-        return 1 + Math.floor(Math.random() * this.numSides);
-    }
-
-    roll({ numRolls }) {
-        const output = [];
-        for (let i = 0; i < numRolls; i++) {
-            output.push(this.rollOnce());
-        }
-        return output;
+class Message {
+    constructor(id, { content, author }) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
     }
 }
 
+const fakeDatabase = {};
+
 export const root = {
-    getDie({ numSides }) {
-        return new RandomDie(numSides || 6);
+    getMessage: ({ id }) => {
+        return fakeDatabase[id];
+    },
+    getMessages: () => {
+        return Object.values(fakeDatabase);
+    },
+    createMessage: ({ input }) => {
+        const id = String(Object.keys(fakeDatabase).length + 1);
+        const message = new Message(id, input);
+        fakeDatabase[id] = message;
+        return message;
+    },
+    updateMessage: ({ id, input }) => {
+        const message = fakeDatabase[id];
+        Object.assign(message, input);
+        return message;
     }
 };
