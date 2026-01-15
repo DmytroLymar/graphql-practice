51882a411a70ae7cf3b8bdb9bfb3976b797dc2c2
Dmytro Lymar <dmytrolymar901@gmail.com>
2026-01-15 09:41:08 +0200

    (education 4.4.3) Object Types

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { createHandler } from 'graphql-http/lib/use/express';
+
+import { root } from './resolvers/root.js';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+
+const schema = loadSchemaSync('src/schema/index.graphql', {
+    loaders: [new GraphQLFileLoader()]
+});
+
+const app = express();
+
+app.all(
+    '/graphql',
+    createHandler({
+        schema: schema,
+        rootValue: root
+    })
+);
+
+// Start the server at port
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
